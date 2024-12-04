7b1123462a26f7a5ed24cd019dc4281bc96fa2f3
samanaziz123 <157869239+samanaziz123@users.noreply.github.com>
2024-12-04 12:56:16 +0500

    Add files via upload

---
+++ blogapp/firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMWoGvZRgpdeSt6vFrJqhX55tQjon4bNw",
+  authDomain: "mini-hackhaton-bb6d7.firebaseapp.com",
+  projectId: "mini-hackhaton-bb6d7",
+  storageBucket: "mini-hackhaton-bb6d7.firebasestorage.app",
+  messagingSenderId: "562325866585",
+  appId: "1:562325866585:web:d04c77ae6419d71a9f8948"
+};
---
+++ blogapp/script.js
@@ -0,0 +1,2 @@
+// Optional JavaScript for further actions (for example, logging user behavior or stats)
+console.log("Welcome Page Loaded!");
